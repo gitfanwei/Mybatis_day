9bb13e4245303dd3c097189d5d7926573930bae9
fanwei <1638206093@qq.com>
2020-06-01 18:42:45 +0800

    带有增删改查的Mybatis

--- src/main/java/com/fan/IUserMapper/UserMapper.java
+++ src/main/java/com/fan/IUserMapper/UserMapper.java
@@ -2,9 +2,17 @@ package com.fan.IUserMapper;
 
 import com.fan.user.User;
 
+import javax.jws.soap.SOAPBinding;
 import java.util.List;
 
 public interface UserMapper {
     //第五步 创建实体类的接口
     List<User> getUserMapper();
+
+    //做一个id查询
+    User getUserID(int id);
+    int insertUser(User user);
+    int getDeleteUser(int idd);
+    int getUpdateUser(User user);
+
 }
--- src/test/java/com/fan/test/testUser.java
+++ src/test/java/com/fan/test/testUser.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.util.List;
 
 public class testUser {
+
     @Test
     public  void test(){
         SqlSession sqlSession = SessFactory.getSqlSession();
@@ -17,5 +18,32 @@ public class testUser {
         for (User user : userMapper) {
             System.out.println(user);
         }
+        sqlSession.close();
     }
+    @Test//向数据库插入数据
+    public  void  test2(){
+        SqlSession sqlSession = SessFactory.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.insertUser(new User("张二","男",91,"白嫖"));
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test //删除数据库中的数据
+    public void test3(){
+        SqlSession sqlSession = SessFactory.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.getDeleteUser(90);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+    @Test //修改表中的数据
+    public  void  test4(){
+        SqlSession sqlSession = SessFactory.getSqlSession();
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        mapper.getUpdateUser(new User("老王","女",91,"买"));
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+
 }
